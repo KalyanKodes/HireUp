2a48062d56e621db66156fed00622ef694bc03a9
Raj kalyan kumar <rajkalyankumar2@gmail.com>
2023-12-14 15:30:36 +0530

    Added final files and all things

--- script.js
+++
@@ -1,5 +0,0 @@
-const arrow = document.querySelector(".bx");
-arrow.addEventListener("click", () => {
-  arrow.classList.toggle("rotateLeft");
-  parent.postMessage({ action: "transformIframe" }, "*");
-});
